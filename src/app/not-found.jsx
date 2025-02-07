98631e4aab3a880960e3d02103a629b37c3fadb5
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-02-06 21:10:04 -0500

    add(404): created a 404 page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,17 @@
+import Transition from "@/components/transition";
+
+import "@/style/notFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="notFoundContainer fadeContent">
+      <img
+        src="/assets/404.svg"
+        alt="404 picture (if you're reading this on a 404 page then something is REALLY wrong)"
+        draggable="false"
+      />
+      <h1>Well $#!%, that's a 404</h1>
+      <p>The page you are looking for does not exist, or has been removed</p>
+    </div>
+  );
+}
