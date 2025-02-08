3c3bdf41cdc152ccf5d620b5052840311d504010
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-02-08 16:07:20 -0500

    update(modPage): ported over 404 to no module found

--- src/app/mod/[slug]/page.jsx
+++ src/app/mod/[slug]/page.jsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+
+import { Home } from "lucide-react";
+
 import Logo from "@/components/logo";
 import BackButton from "@/components/backButton";
 import MobileHeader from "@/components/mobileHeader";
@@ -5,6 +9,7 @@ import HeaderButton from "@/components/headerButton";
 
 import modules from "@/distData/modules.json";
 
+import "@/style/notFound.css";
 import "@/style/modPage.css";
 
 export default async function Page({ params }) {
@@ -13,7 +18,24 @@ export default async function Page({ params }) {
   const moduleFound = modules.find((module) => module.slug === slug);
 
   if (!moduleFound) {
-    return <h1>Module Not Found (Placeholder design)</h1>;
+    return (
+      <div className="notFoundContainer fadeContent">
+        <img
+          src="/assets/404.svg"
+          alt="404 picture (if you're reading this on a 404 page then something is REALLY wrong)"
+          draggable="false"
+        />
+        <h1>Well $#!%, that's a 404</h1>
+        <p>
+          The module or page you are looking for does not exist, or has been
+          removed.
+        </p>
+        <Link href="/">
+          <Home />
+          Bring me home please :(
+        </Link>
+      </div>
+    );
   }
 
   const module = moduleFound;
