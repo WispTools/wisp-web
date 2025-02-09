013be945a880d28d0cc26531cfd444e61036fecf
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-02-08 21:15:34 -0500

    update(mod): optimized blank mod redirect

--- src/app/mod/page.jsx
+++ src/app/mod/page.jsx
@@ -1,10 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { useEffect } from "react";
-
-export default function App() {
-  useEffect(() => {
-    window.location.href = "/";
-  }, []);
-  return <></>;
+export default async function Discord() {
+  redirect(`/`);
 }
