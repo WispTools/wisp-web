faeb2025f39c755126a420dc473bb0008eeef091
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-02-09 14:20:00 -0500

    add(contribute): updated contribute page

--- src/app/contribute/page.jsx
+++ src/app/contribute/page.jsx
@@ -1,12 +1,55 @@
 import Link from "next/link";
 
+import {
+  CodeXml,
+  BookOpen,
+  MessageSquare,
+  GitPullRequestArrow,
+} from "lucide-react";
+
+import "@/style/contribute.css";
+import "@/style/home.css";
+
 export default function Contribute() {
   return (
-    <div className="fadeContent">
-      <h1>Contribute</h1>
-      <Link href="/discord" target="_blank">
-        Discord Server
-      </Link>
+    <div className="contributeContainer fadeContent">
+      <h1>Contributing</h1>
+      <div className="contributeCard">
+        <button className="modLink">
+          <span className="modLinkIcon">
+            <GitPullRequestArrow />
+          </span>
+          <div className="modLinkInfo">
+            <h3>Your Contribution Here!</h3>
+            <p>It's easier than you think, trust me.</p>
+          </div>
+        </button>
+        <hr />
+        <p>
+          If you are interested in contributing to Wisp, we would love to have
+          you! All you need to know is the basics of JavaScript and HTML. You
+          don't need to be an expert programmer, just try your best and if you
+          have any problems we will help you out along the way!
+        </p>
+      </div>
+      <div className="contributeLinks">
+        <Link
+          href="https://github.com/WispTools"
+          target="_blank"
+          className="contributeLink"
+        >
+          <CodeXml />
+          Repositories
+        </Link>
+        <Link href="/docs" target="_blank" className="contributeLink">
+          <BookOpen />
+          Documentation
+        </Link>
+        <Link href="/discord" target="_blank" className="contributeLink">
+          <MessageSquare />
+          Discord
+        </Link>
+      </div>
     </div>
   );
 }
---
+++ src/app/docs/page.jsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default async function Discord() {
+  redirect(`https://wisptools.gitbook.io/wisp`);
+}
