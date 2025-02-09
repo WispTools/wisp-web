c8708434674a3cbf62668b7ca22ede5afa8cf480
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-02-09 14:25:42 -0500

    update(contribute): updated styling

--- src/app/contribute/page.jsx
+++ src/app/contribute/page.jsx
@@ -13,8 +13,8 @@ import "@/style/home.css";
 export default function Contribute() {
   return (
     <div className="contributeContainer fadeContent">
-      <h1>Contributing</h1>
       <div className="contributeCard">
+        <h1>Contributing</h1>
         <button className="modLink">
           <span className="modLinkIcon">
             <GitPullRequestArrow />
@@ -24,31 +24,30 @@ export default function Contribute() {
             <p>It's easier than you think, trust me.</p>
           </div>
         </button>
-        <hr />
-        <p>
+        <p className="contributeCard">
           If you are interested in contributing to Wisp, we would love to have
           you! All you need to know is the basics of JavaScript and HTML. You
           don't need to be an expert programmer, just try your best and if you
           have any problems we will help you out along the way!
         </p>
-      </div>
-      <div className="contributeLinks">
-        <Link
-          href="https://github.com/WispTools"
-          target="_blank"
-          className="contributeLink"
-        >
-          <CodeXml />
-          Repositories
-        </Link>
-        <Link href="/docs" target="_blank" className="contributeLink">
-          <BookOpen />
-          Documentation
-        </Link>
-        <Link href="/discord" target="_blank" className="contributeLink">
-          <MessageSquare />
-          Discord
-        </Link>
+        <div className="contributeLinks">
+          <Link
+            href="https://github.com/WispTools"
+            target="_blank"
+            className="contributeLink"
+          >
+            <CodeXml />
+            Repositories
+          </Link>
+          <Link href="/docs" target="_blank" className="contributeLink">
+            <BookOpen />
+            Documentation
+          </Link>
+          <Link href="/discord" target="_blank" className="contributeLink">
+            <MessageSquare />
+            Discord
+          </Link>
+        </div>
       </div>
     </div>
   );
