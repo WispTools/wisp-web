ac771b744c14a21e03abb36ae528d66f3425b599
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-02-14 22:27:46 -0500

    update(credits): removed bloat

--- src/app/settings/credits/page.jsx
+++ src/app/settings/credits/page.jsx
@@ -18,14 +18,8 @@ export default function Credits() {
       </MobileHeader>
       <div className="credits fadeContent">
         <div>
-          <span className="creditsLabel">Creator</span>
+          <span className="creditsLabel">Creator & Maintainer</span>
           <p>Ethan Hazel</p>
-          <span className="creditsLabel">Maintainers</span>
-          <p>Ethan Hazel</p>
-          <span className="creditsLabel">Brand Assets & Design</span>
-          <p>Ethan Hazel</p>
-          <span className="creditsLabel">License</span>
-          <p>GPLv3</p>
           <span className="creditsLabel">Icon Designs</span>
           <a href="https://lucide.dev/" target="_blank">
             Lucide
