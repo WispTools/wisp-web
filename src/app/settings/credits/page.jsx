c7ed0b00e403de6b0804245e3200630fd9b672de
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2025-02-14 19:13:04 -0500

    update(credits): added mobileheader

--- src/app/settings/credits/page.jsx
+++ src/app/settings/credits/page.jsx
@@ -1,34 +1,47 @@
+import MobileHeader from "@/components/mobileHeader";
+import { HeaderSpacer } from "@/components/headerButton";
 import Contributors from "@/components/contributors";
+import Logo from "@/components/logo";
+import BackButton from "@/components/backButton";
 
 import "@/style/credits.css";
 
 export default function Credits() {
   return (
-    <div className="credits fadeContent">
-      <div>
-        <span className="creditsLabel">Creator</span>
-        <p>Ethan Hazel</p>
-        <span className="creditsLabel">Maintainers</span>
-        <p>Ethan Hazel</p>
-        <span className="creditsLabel">Brand Assets & Design</span>
-        <p>Ethan Hazel</p>
-        <span className="creditsLabel">License</span>
-        <p>GPLv3</p>
-        <span className="creditsLabel">Icon Designs</span>
-        <a href="https://lucide.dev/" target="_blank">
-          Lucide
+    <>
+      <MobileHeader>
+        <BackButton />
+        <a href="/" className="logo">
+          <Logo size={"48px"} />
         </a>
-      </div>
-      <span className="creditsDivider"></span>
-      <div>
-        <span className="creditsLabel">Special Thanks</span>
-        <p>Elizabeth Grier</p>
-        <p>Anthony Frye</p>
-        <span className="creditsLabel">Contributors</span>
-        <div className="creditsContributors">
-          <Contributors />
+        <HeaderSpacer />
+      </MobileHeader>
+      <div className="credits fadeContent">
+        <div>
+          <span className="creditsLabel">Creator</span>
+          <p>Ethan Hazel</p>
+          <span className="creditsLabel">Maintainers</span>
+          <p>Ethan Hazel</p>
+          <span className="creditsLabel">Brand Assets & Design</span>
+          <p>Ethan Hazel</p>
+          <span className="creditsLabel">License</span>
+          <p>GPLv3</p>
+          <span className="creditsLabel">Icon Designs</span>
+          <a href="https://lucide.dev/" target="_blank">
+            Lucide
+          </a>
+        </div>
+        <span className="creditsDivider"></span>
+        <div>
+          <span className="creditsLabel">Special Thanks</span>
+          <p>Elizabeth Grier</p>
+          <p>Anthony Frye</p>
+          <span className="creditsLabel">Contributors</span>
+          <div className="creditsContributors">
+            <Contributors />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
